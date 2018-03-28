b3edb7c420f8c3f5c267053769dd316471e606b3
rainbowvs <527532711@qq.com>
2018-03-29 03:34:18 +0800

    添加路由拦截

--- src/main.js
+++ src/main.js
@@ -18,14 +18,52 @@ Vue.component('my-warn',Warn);
 Vue.component('my-dialog',Dialog);
 Vue.config.productionTip = true;
 
+window.reqUrl = 'http://192.168.1.101:8082/interface/';
+//window.reqUrl = '../interface/';
+
 //[全局]进入前触发
 router.beforeEach((to, from, next) => {
-//	store.commit('TOGGLE_LOADING',true);
 	if(to.meta.requireProp){
-		if(window.localStorage.getItem('yw_token')){
-			next();
-		}else{
+		if(!(store.state.adminInfo.hasOwnProperty('token') && store.state.adminInfo['token']))
 			next({name: 'Login'});
+		else{
+			if(to.name == 'Administrator'){
+				ajax({
+					name: '验证管理员权限',
+					url: window.reqUrl + 'ywms_admin.php',
+					data: {
+						handle: 'check',
+						uid: store.state.adminInfo['id'],
+						token: store.state.adminInfo['token'],
+					},
+					beforeSend () {
+						store.commit('TOGGLE_LOADING',true);
+					}
+				}).then(res => {
+					if(res.type == 'success'){
+						next();
+					}else{
+						if(res.status == 1)
+							setTimeout(() => {
+								that.$store.commit('DEL_ADMININFO');
+								router.push({name: 'Login'});
+							},1000);
+						store.commit('TOGGLE_WARNING',{
+							msg: res.msg,
+							visibility: true,
+						});
+					}
+					store.commit('TOGGLE_LOADING',false);
+				}).catch(status => {
+					store.commit('TOGGLE_LOADING',false);
+					store.commit('TOGGLE_WARNING',{
+						msg: status,
+						visibility: true,
+					});
+				});
+			}else{
+				next();
+			}
 		}
 	}else{
 		next(); // 确保一定要调用 next()
@@ -35,6 +73,14 @@ router.beforeEach((to, from, next) => {
 router.afterEach((to, from) => {
 	store.commit('TOGGLE_LOADING',false);
 });
+
+/*注释*/
+console.groupCollapsed('%c隐藏在角落的发光宝箱','color:gold;');
+console.log('%c@author PZJ','font-size:16px;color:deepskyblue');
+console.log('%c@version 1.0.0','font-size:16px;');
+console.log('%c@date 2018-03-21','font-size:16px;');
+console.log('仍有非常多的功能未完善，待续...');
+console.groupEnd();
 
 /* eslint-disable no-new */
 new Vue({
